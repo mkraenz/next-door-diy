a5d8d012c392b5c93da2e69870b5b83fb7f9d65a
Mirco Kraenz <software@kraenz.eu>
2023-12-15 18:30:00 +0100

    chore(#4): test setup

---
+++ apps/admin-web/src/__tests__/test-utils.tsx
@@ -0,0 +1,37 @@
+import { Store } from '@reduxjs/toolkit';
+import { cleanup, render } from '@testing-library/react';
+import { I18nextProvider, I18nextProviderProps } from 'react-i18next';
+import { Provider } from 'react-redux';
+import { afterEach } from 'vitest';
+import createI18n from '../localization/i18n';
+
+let i18n: I18nextProviderProps['i18n'];
+beforeEach(async () => {
+  i18n = createI18n();
+  await i18n.init();
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+function customRender(ui: React.ReactElement, options: { store?: Store } = {}) {
+  return render(ui, {
+    // wrap provider(s) here if needed
+    wrapper: ({ children }) => {
+      if (options.store)
+        return (
+          <Provider store={options.store}>
+            <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+          </Provider>
+        );
+      return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+    },
+    ...options,
+  });
+}
+
+export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
+// override render export
+export { customRender as render };
--- apps/admin-web/vite.config.ts
+++ apps/admin-web/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
@@ -6,5 +9,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
+    mockReset: true,
+    clearMocks: true,
+    restoreMocks: true,
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    // css: true,
   },
 });
